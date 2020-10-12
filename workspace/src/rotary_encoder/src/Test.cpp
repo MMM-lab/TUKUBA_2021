fde092fc93de4c1a0e09a1fb89bb8d174dbb4852
Ramune6110 <arduino6110@gmail.com>
2020-10-12 07:16:59 -0700

    [add] Test

--- workspace/src/rotary_encoder/src/Test.cpp
+++ workspace/src/rotary_encoder/src/Test.cpp
@@ -1,149 +1,247 @@
 #include <ros/ros.h>
-#include <std_msgs/Int32MultiArray.h>
-#include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Int8MultiArray.h>
+#include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
 
+/***********************************************************************
+ * Global variables
+ **********************************************************************/
 // HajimeCart parameter
-const double wheel_radius= 0.085;   // wr
-const double base_width = 0.236;    // ws
-const double _pi = 3.141592654;
-const double xp_enc_ticks_meter = 6250.0 / (2.0 * _pi * wheel_radius);
+const double wheel_radius           = 0.095f;   
+const double base_width             = 0.340f; 
+const float wheel_to_wheel_distance = 0.310f;
 
-double encoder_left = 0;
-double encoder_right = 0;
-double encoder_left_old = 0;
-double encoder_right_old = 0;
-double pos_x = 0.0;
-double pos_y = 0.0;
-double pos_th = 0.0;
+// ロータリーエンコーダの1回転あたりのパルス数
+int rotary_encoder_resolution = 100;
 
-int flag_firsttime = 1;
+// ロータリーエンコーダの回転数を保持. 1回転で400増加
+int encoder_value_left;
+int encoder_value_right;
 
-void encoderCallback(const std_msgs::Int32MultiArray::ConstPtr& msg);
+// 現時刻でのロータリーエンコーダの角度を保持
+float rotation_angle_left;
+float rotation_angle_right;
+
+// 前時刻でのロータリーエンコーダの角度を保持
+float pre_rotation_angle_left = 0.0f;
+float pre_rotation_angle_right = 0.0f;
+
+// 現時刻でのロータリーエンコーダの角速度を保持
+float rotation_angular_velocity_left;
+float rotation_angular_velocity_right;
+
+// 両車輪の移動速度
+float left_vel  = 0.0f;
+float right_vel = 0.0f;
+
+// ロボットの並進速度
+float linear_vel = 0.0f;
+// ロボットの回転速度
+float angular_vel = 0.0f;
+
+// ロボットの位置と姿勢
+float x     = 0.0f;
+float y     = 0.0f;
+float theta = 0.0f;
 
 ros::Publisher odom_pub;
+ros::Publisher cmdmotorspeed_pub;
+ros::Time last_time;
+ros::Time current_time;
 
-int main(int argc, char** argv){
-    ros::init(argc, argv, "odometry_publisher");
-    ros::NodeHandle n;
+// odom
+nav_msgs::Odometry odom;
+geometry_msgs::TransformStamped odom_trans;
+//tf::TransformBroadcaster odom_broadcaster;
 
-    ros::Subscriber encoder_sub = n.subscribe("encoder", 10, encoderCallback);
-    odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+// 制御周期
+float dt; //sec
 
-    ros::spin(); // check for incoming messages
+// PWM出力のパルス幅(0~100で指定する)
+int pwm_width_left;
+int pwm_width_right;
+//std_msgs::Int32MultiArray pwm_width_data;
+std_msgs::Int8MultiArray pwm_width_data;
 
-    return 0;
+// モーター出力電圧に加えるオフセット値(摩擦トルクを相殺するために用いる)
+float motor_offset = 0.17; //volt
+
+// 左右のモータへの電圧
+float delta_speed_left;
+float delta_speed_right;
+float i_left;
+float i_right;
+float ki = 0.00001;
+float kp = 1;
+float motor_left_value;
+float motor_right_value;
+
+/***********************************************************************
+ * Callback function
+ **********************************************************************/
+
+void cmd_vel_callback(const geometry_msgs::Twist& msg) {
+  rotation_angular_velocity_left  = (msg.linear.x - msg.angular.z * base_width / 2.0) / wheel_radius;    // left motor [rad/s]
+  rotation_angular_velocity_right = (msg.linear.x + msg.angular.z * base_width / 2.0) / wheel_radius;    // right motor [rad/s]
 }
-
-ros::Time current_time, last_time;
 
 void encoderCallback(const std_msgs::Int32MultiArray::ConstPtr& msg)
 {
-    static tf::TransformBroadcaster odom_broadcaster;
+    encoder_value_left  = msg->data[0];
+    encoder_value_right = msg->data[1];
+}
 
-    encoder_left = msg->data[0];
-    encoder_right = msg->data[1];
+/**
+ *****************************************************************************************************
+ * MAIN METHOD
+ *****************************************************************************************************
+*/
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "cmdvel_cmdmotorspeed");
+  ros::NodeHandle nh;
+  
+  pwm_width_data.data.resize(2);
+  pwm_width_data.data[0] = 0;
+  pwm_width_data.data[1] = 0;
 
-    if( flag_firsttime )
-    {
-        flag_firsttime = 0;
+  // publisher
+  odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 10);
+  //cmdmotorspeed_pub = nh.advertise<std_msgs::Int32MultiArray>("cmdmotorspeed", 10);
+  cmdmotorspeed_pub = nh.advertise<std_msgs::Int8MultiArray>("cmdmotorspeed", 10);
 
-        encoder_left_old = encoder_left;
-        encoder_right_old = encoder_right;
+  // sbscriber
+  ros::Subscriber encoder_sub = nh.subscribe("encoder", 10, encoderCallback);
+  ros::Subscriber cmd_vel_sub = nh.subscribe("cmd_vel", 10, cmd_vel_callback);
+  
+  // time 
+  last_time    = ros::Time::now();
+  current_time = ros::Time::now();
 
-	last_time = ros::Time::now();
+  ros::Rate loop_rate(10);	//10Hz(10ms)
 
-        return;
-    }
-
+  while (ros::ok()) {
     current_time = ros::Time::now();
 
-    //compute odometry in a typical way given the velocities of the robot
-//    double dt=((double)current_time.sec + 1e-9*(double)current_time.nsec) - ((double)last_time.sec + 1e-9*(double)last_time.nsec);
-    double dt = (current_time - last_time).toSec();
+    dt = (current_time - last_time).toSec();
 
-    double d_left = (encoder_left -  encoder_left_old ) / xp_enc_ticks_meter;
-    double d_right = (encoder_right -  encoder_right_old ) / xp_enc_ticks_meter;
+    /**
+    ***********************************************************************
+    * Odometry
+    ***********************************************************************
+    */
+    //-------------------------------------------
+    // タイヤの回転角
+    //-------------------------------------------
+    rotation_angle_left  = encoder_value_left * (2 * 3.14f)/(4 * rotary_encoder_resolution);
+    rotation_angle_right = encoder_value_right * (2 * 3.14f)/(4 * rotary_encoder_resolution);
 
-    encoder_left_old = encoder_left;
-    encoder_right_old = encoder_right;
+    //-------------------------------------------
+    // タイヤの回転速度
+    //-------------------------------------------
+    rotation_angular_velocity_left  = (rotation_angle_left - pre_rotation_angle_left) / dt;
+    rotation_angular_velocity_right = (rotation_angle_right - pre_rotation_angle_right) / dt;
 
-    double d = (d_left + d_right) / 2.0;
-    double th = (d_right - d_left) / base_width;
+    //-------------------------------------------
+    // ロボットの移動速度
+    //-------------------------------------------
+    left_vel    = rotation_angular_velocity_left * wheel_radius;
+    right_vel   = rotation_angular_velocity_right * wheel_radius;
+    linear_vel  = (right_vel + left_vel) / 2.0f;
+    angular_vel = (right_vel - left_vel) / wheel_to_wheel_distance;
 
-    // calc dt becomes big for the first time
-    double dx;
-    double dr;
-    if( dt < 1000.0 ) 
-    {
-        dx = d / dt;
-        dr = th / dt;
-    }
-    else
-    {
-        dt = 0.0;
-        dx = 0;
-        dr = 0;
-    }
+    //-------------------------------------------
+    // ロボットの位置
+    //-------------------------------------------
+    x     = x + linear_vel * dt * cos(theta + angular_vel / 2.0f);
+    y     = y + linear_vel * dt * sin(theta + angular_vel / 2.0f);
+    theta = theta + angular_vel * dt;
 
-    if( fabs(d) > 1.0e-5 )
-    {
-        double _x = cos(th) * d;
-        double _y = -sin(th) * d;
-        pos_x += (_x * cos(pos_th) - _y * sin(pos_th));
-        pos_y += (_x * sin(pos_th) + _y * cos(pos_th));
-    }
-    if(fabs(th) > 1.0e-5 )
-    {
-        pos_th += th;
-    }
-
-    // const double wr= 0.085;
-    // const double ws = 0.236;
-    //    r =  ws/2.0 * (encoder_left + encoder_right)/(encoder_right - encoder_left );
-    //  omega_dt = (encoder_right - encoder_left) * step / ws
-    //  iccx = x - r * sin(theta)
-    //  iccy = y + r * cos(theta)
-    //  x_new = (x - iccx) * cos(omega_dt) - (y - iccy) * sin(omega_dt) + iccx;
-    //  y_new = (x - iccx) * sin(omega_dt) + (y - iccy) * cos(omega_dt) + iccy;
-    //  theta_new = theta + omega_dt;
-
+    static tf::TransformBroadcaster odom_broadcaster;
     //since all odometry is 6DOF we'll need a quaternion created from yaw
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pos_th);
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta);
 
     //first, we'll publish the transform over tf
-    geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "/odom";
-    odom_trans.child_frame_id = "/base_footprint";
+    odom_trans.header.stamp    = current_time;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id  = "base_link";
 
-    odom_trans.transform.translation.x = pos_x;
-    odom_trans.transform.translation.y = pos_y;
+    odom_trans.transform.translation.x = x;
+    odom_trans.transform.translation.y = y;
     odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation = odom_quat;
+    odom_trans.transform.rotation      = odom_quat;
 
     //send the transform
     odom_broadcaster.sendTransform(odom_trans);
 
     //next, we'll publish the odometry message over ROS
-    nav_msgs::Odometry odom;
     odom.header.stamp = current_time;
-    odom.header.frame_id = "/odom";
+    odom.header.frame_id = "odom";
 
     //set the position
-    odom.pose.pose.position.x = pos_x;
-    odom.pose.pose.position.y = pos_y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
+    odom.pose.pose.position.x  = x;
+    odom.pose.pose.position.y  = y;
+    odom.pose.pose.position.z  = 0.0;
+    odom.pose.pose.orientation = odom_quat ;
 
     //set the velocity
-    odom.child_frame_id = "/base_footprint";
-    odom.twist.twist.linear.x = dx;
-    odom.twist.twist.linear.y = 0;
-    odom.twist.twist.angular.z = dr;
+    odom.child_frame_id = "base_link";
+    odom.twist.twist.linear.x  = linear_vel;
+    odom.twist.twist.angular.z = angular_vel;
+
+    /**
+    ***********************************************************************
+    * Moter Control
+    ***********************************************************************
+    */
+    
+    // P制御
+    delta_speed_left  = rotation_angular_velocity_left - rotation_angular_velocity_left;
+    delta_speed_right = rotation_angular_velocity_right - rotation_angular_velocity_right;
+    // I制御
+    i_left += delta_speed_left * dt;
+    i_right += delta_speed_right * dt;
+    // PI制御
+    motor_left_value  = kp * delta_speed_left + ki * i_left;
+    motor_right_value = kp * delta_speed_right + ki * i_right;
+    
+    // motor_left_value
+    if(motor_left_value > 0)
+    {
+        motor_left_value += motor_offset;   
+    }
+    if(motor_left_value < 0)
+    {
+        motor_left_value -= motor_offset;    
+    }
+
+    // motor_right_value
+    if(motor_right_value > 0)
+    {
+        motor_right_value += motor_offset;   
+    }
+    if(motor_right_value < 0)
+    {
+        motor_right_value -= motor_offset;    
+    }
+    
+    //calculate PWM pulse width
+    pwm_width_left  = int(motor_left_value * 100.0f / 3.3f );
+    pwm_width_right = int(motor_right_value * 100.0f / 3.3f );
+    pwm_width_data.data[0] = pwm_width_left;
+    pwm_width_data.data[1] = pwm_width_right;
 
     //publish the message
     odom_pub.publish(odom);
+    cmdmotorspeed_pub.publish(pwm_width_data);
 
     last_time = current_time;
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
 }
--- workspace/src/rotary_encoder/src/simple_publish_encoder_sbscribe_PWM.cpp
+++ workspace/src/rotary_encoder/src/simple_publish_encoder_sbscribe_PWM.cpp
@@ -1,9 +1,9 @@
 /*
- *******************************************************************************
+  *******************************************************************************
  * @file PI control pwm
- * @brief read two encoder value
+ * @brief cmd_vel to pwm value
  * @author Ramune6110
- * @date 2020 10/11
+ * @date 2020 10/06
  * SYSTEM            | NUCLEO-F401RE
  * Motor driver      | TA7291P x 2
  * Rotary encoder    | EC202A100A x 2
@@ -22,25 +22,51 @@
 #include <ros.h>
 #include <std_msgs/Int8MultiArray.h>
 #include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
 
-/* ROS */
-ros::NodeHandle  nh;
+/***********************************************************************
+ * Global variables
+ **********************************************************************/
 
-// Encoder
+/* ROS communication*/
+ros::NodeHandle nh;
+
+float motor_left_ref  = 0.0f;
+float motor_right_ref = 0.0f;
+float rotation_angular_velocity_left  = 0.0f;
+float rotation_angular_velocity_right = 0.0f;
+
+/*void motor_speed_Cb(const std_msgs::Float32MultiArray& msg)
+{
+  motor_left_ref  = msg.data[0];
+  motor_right_ref = msg.data[1];
+}
+
+void rotation_vel_Cb(const std_msgs::Float32MultiArray& msg)
+{
+  rotation_angular_velocity_left  = msg.data[0];
+  rotation_angular_velocity_right = msg.data[1];
+}*/
+
+// publish
 std_msgs::Int32MultiArray encoder_data;
 ros::Publisher encoder_pub("encoder", &encoder_data);
+
+// subscribe
+//ros::Subscriber<std_msgs::Float32MultiArray> cmdmotorspeed_sub("cmdmotorspeed", &motor_speed_Cb);
+//ros::Subscriber<std_msgs::Float32MultiArray> rotation_angular_velocity_sub("rotation_angular_velocity", &rotation_vel_Cb);
 
 // PWM
 // PWM出力のパルス幅(0~100で指定する)
 int pwm_width_left;
 int pwm_width_right;
-/*void motor_speed_Cb(const std_msgs::Int32MultiArray& msg)
-{
-  pwm_width_left  = msg.data[0];
-  pwm_width_right = msg.data[1];
-}
-
-ros::Subscriber<std_msgs::Int32MultiArray> cmdmotorspeed_sub("cmdmotorspeed", &motor_speed_Cb);*/
+//void motor_speed_Cb(const std_msgs::Int32MultiArray& msg)
+//{
+//  pwm_width_left  = msg.data[0];
+//  pwm_width_right = msg.data[1];
+//}
+//
+//ros::Subscriber<std_msgs::Int32MultiArray> cmdmotorspeed_sub("cmdmotorspeed", &motor_speed_Cb);
 
 void motor_speed_Cb(const std_msgs::Int8MultiArray& msg)
 {
@@ -49,14 +75,6 @@ void motor_speed_Cb(const std_msgs::Int8MultiArray& msg)
 }
 
 ros::Subscriber<std_msgs::Int8MultiArray> cmdmotorspeed_sub("cmdmotorspeed", &motor_speed_Cb);
-
-/***********************************************************************
- * Prototype declaration
- **********************************************************************/
-void move_forward();
-void move_backwaed();
-void move_turn_right();
-void move_turn_left();
 
 /***********************************************************************
  * Encoder variables
@@ -85,7 +103,7 @@ Ticker timer_encoder; //for rotary encoder
 
 //=========================================================
 // ロータリーエンコーダの出力を確認する時間間隔
-int rotary_encoder_update_rate = 25; //usec
+int rotary_encoder_update_rate = 10000; //usec
 
 // ロータリーエンコーダの回転数を保持. 1回転で400増加
 int encoder_value_left  = 0;
@@ -95,6 +113,9 @@ int encoder_value_right = 0;
 int table[16] = {0, 1, -1, 0,  -1, 0, 0, 1,  1, 0, 0, -1,  0, -1, 1, 0};
 
 //=========================================================
+// モーターの制御周期
+float feedback_rate = 0.01; //sec
+
 // モーターの回転方向(1なら正転, 2なら反転)
 int motor_direction_left  = 1;
 int motor_direction_right = 1;
@@ -103,6 +124,12 @@ int motor_direction_right = 1;
 float motor_offset = 0.17; //volt
 
 // 左右のモータへの電圧
+float delta_speed_left  = 0.0f;
+float delta_speed_right = 0.0f;
+float i_left  = 0.0f;
+float i_right = 0.0f;
+float ki = 0.00001f;
+float kp = 1.0f;
 float motor_left_value  = 0.0f;
 float motor_right_value = 0.0f;
 
@@ -118,8 +145,8 @@ void rotary_encoder_check()
     static int code_left; 
     static int code_right; 
     //check the movement
-    code_left  = ( (code_left<<2) +  int(encoder_bus_left) ) & 0xf ;
-    code_right = ( (code_right<<2) +  int(encoder_bus_right) ) & 0xf ;
+    code_left  = ( (code_left<<2) + int(encoder_bus_left) ) & 0xf ;
+    code_right = ( (code_right<<2) + int(encoder_bus_right) ) & 0xf ;
     //update the encoder value
     int value_left  = -1 * table[code_left];
     int value_right = -1 * table[code_right];
@@ -127,24 +154,26 @@ void rotary_encoder_check()
     encoder_value_left += value_left;
     encoder_value_right += value_right;
 
-    // 左右のエンコーダ値をpublishする
+    // encoder publish
     encoder_data.data[0] = encoder_value_left;
     encoder_data.data[1] = encoder_value_right; 
     encoder_pub.publish(&encoder_data);
+
+    return;
 }
 
 void ros_init()
 {
-    /* ROS */
     encoder_data.data_length = 2;
     encoder_data.data = (int32_t *)malloc(sizeof(int32_t)*2);
-    encoder_data.data[0] = 0;
-    encoder_data.data[1] = 0;
+    encoder_data.data[0] = 0.0f;
+    encoder_data.data[1] = 0.0f;
 
-    //nh.getHardware()->setBaud(230400);
+    nh.getHardware()->setBaud(921600);
     nh.initNode();
     nh.advertise(encoder_pub);
     nh.subscribe(cmdmotorspeed_sub);
+    //nh.subscribe(rotation_angular_velocity_sub);
 }
 
 /**
@@ -159,10 +188,8 @@ int main() {
     //-------------------------------------------
     // System initialization
     //-------------------------------------------
-    // シリアル通信の速度設定
-    //pc.baud(115200);
     ros_init();
-
+     
     //-------------------------------------------
     // Motor driver intialization
     //-------------------------------------------
@@ -170,8 +197,8 @@ int main() {
     IN2_left  = 0; //motor stop
     IN1_right = 0; //motor stop
     IN2_right = 0; //motor stop
-    motor_left.period_us(100);    //10 kHz pulse
-    motor_right.period_us(100);   //10 kHz pulse
+    motor_left.period_us(10);    //10 kHz pulse
+    motor_right.period_us(10);   //10 kHz pulse
     motor_left.pulsewidth_us(0);  //0 to 100
     motor_right.pulsewidth_us(0); //0 to 100
 
@@ -188,185 +215,153 @@ int main() {
     */
     while(1)
     {
-        /***********************************************************************
-        * moter PWM
+        /**********************************************************************
+        * PI Moter Control
         **********************************************************************/
-        if(pwm_width_left >= 0 && pwm_width_right >= 0) {
-            move_forward();
-        } else if (pwm_width_left < 0 && pwm_width_right < 0) {
-            move_backwaed();
-        } else if (pwm_width_left >= 0 && pwm_width_right < 0) {
-            move_turn_right();
-        } else if (pwm_width_left < 0 && pwm_width_right >= 0) {
-            move_turn_left();
+        /*// P制御
+        delta_speed_left  = motor_left_ref - rotation_angular_velocity_left;
+        //delta_speed_right = -cmdmotorspeed.data[1] - rotation_angular_velocity_right;
+        delta_speed_right = motor_right_ref - rotation_angular_velocity_right;
+        // I制御
+        i_left += delta_speed_left * feedback_rate;
+        i_right += delta_speed_right * feedback_rate;
+        // PI制御
+        motor_left_value  = kp * delta_speed_left + ki * i_left;
+        motor_right_value = kp * delta_speed_right + ki * i_right;
+        
+        // motor_left_value
+        if(motor_left_value > 0)
+        {
+            motor_left_value += motor_offset;   
+        }
+        if(motor_left_value < 0)
+        {
+            motor_left_value -= motor_offset;    
+        }
+
+        // motor_right_value
+        if(motor_right_value > 0)
+        {
+            motor_right_value += motor_offset;   
+        }
+        if(motor_right_value < 0)
+        {
+            motor_right_value -= motor_offset;    
+        }
+        
+        //calculate PWM pulse width
+        pwm_width_left  = int(motor_left_value * 100.0f / 3.3f );
+        pwm_width_right = int(motor_right_value * 100.0f / 3.3f );*/
+
+        /***********************************************************************
+        * left moter PWM
+        **********************************************************************/
+        //drive the motor in forward
+        if(pwm_width_left>=0)
+        {
+            //over voltage
+            if(pwm_width_left>100)
+            {
+                pwm_width_left = 100;    
+            }         
+            //to protect TA7291P
+            if(motor_direction_left == 2)
+            {
+                IN1_left = 0;
+                IN2_left = 0;
+                wait(0.0001); //wait 100 usec    
+            }        
+            //forward
+            IN1_left = 1;
+            IN2_left = 0;
+            
+            motor_left.pulsewidth_us(pwm_width_left);
+            motor_direction_left = 1;
+        }      
+        //drive the motor in reverse
+        else
+        {
+            //calculate the absolute value
+            pwm_width_left = -1 * pwm_width_left;
+
+            //over voltage
+            if(pwm_width_left>100)
+            {
+                pwm_width_left = 100;    
+            }
+            //to protect TA7291P
+            if(motor_direction_left == 1)
+            {
+                IN1_left = 0;
+                IN2_left = 0;
+                wait(0.0001); //wait 100 usec    
+            }
+            //reverse
+            IN1_left = 0;
+            IN2_left = 1;
+
+            motor_left.pulsewidth_us(pwm_width_left);
+            motor_direction_left = 2;          
+        }
+
+        /***********************************************************************
+        * right moter PWM
+        **********************************************************************/
+        //drive the motor in forward
+        if(pwm_width_right>=0)
+        {
+            //over voltage
+            if(pwm_width_right>100)
+            {
+                pwm_width_right = 100;    
+            }         
+            //to protect TA7291P
+            if(motor_direction_right == 2)
+            {
+                IN1_right = 0;
+                IN2_right = 0;
+                wait(0.0001); //wait 100 usec    
+            }        
+            //forward
+            IN1_right = 1;
+            IN2_right = 0;
+            
+            motor_right.pulsewidth_us(pwm_width_right);
+            motor_direction_right = 1;
+        }      
+        //drive the motor in reverse
+        else
+        {
+            //calculate the absolute value
+            pwm_width_right = -1 * pwm_width_right;
+
+            //over voltage
+            if(pwm_width_right>100)
+            {
+                pwm_width_right = 100;    
+            }
+            //to protect TA7291P
+            if(motor_direction_right == 1)
+            {
+                IN1_right = 0;
+                IN2_right = 0;
+                wait(0.0001); //wait 100 usec    
+            }
+            //reverse
+            IN1_right = 0;
+            IN2_right = 1;
+
+            motor_right.pulsewidth_us(pwm_width_right);
+            motor_direction_right = 2;          
         }
 
         // wait 
-        wait_ms(10);    
+        //__disable_irq();
+        // encoder_data.data[0] = encoder_value_left;
+        // encoder_data.data[1] = encoder_value_right; 
+        // encoder_pub.publish(&encoder_data);
+        nh.spinOnce();  
+        //__enable_irq();
+        wait_ms(1000);    
     }    
-}
-
-/***********************************************************************
-* moter control
-**********************************************************************/
-void move_forward()
-{
-    //over voltage
-    if(pwm_width_left > 100)
-    {
-        pwm_width_left = 100;    
-    }    
-    if(pwm_width_right > 100)
-    {
-        pwm_width_right = 100;    
-    }         
-
-    //to protect TA7291P
-    if(motor_direction_left == 2)
-    {
-        IN1_left = 0;
-        IN2_left = 0;
-        wait(0.0001); //wait 100 usec    
-    }    
-    if(motor_direction_right == 2)
-    {
-        IN1_right = 0;
-        IN2_right = 0;
-        wait(0.0001); //wait 100 usec    
-    }        
-
-    //forward
-    IN1_left  = 1;
-    IN2_left  = 0;
-    IN1_right = 1;
-    IN2_right = 0;
-    
-    motor_left.pulsewidth_us(pwm_width_left);
-    motor_right.pulsewidth_us(pwm_width_right);
-    motor_direction_left  = 1;
-    motor_direction_right = 1;
-}
-
-void move_backwaed()
-{
-    //calculate the absolute value
-    pwm_width_left = -1 * pwm_width_left;
-    pwm_width_right = -1 * pwm_width_right;
-
-    //over voltage
-    if(pwm_width_left>100)
-    {
-        pwm_width_left = 100;    
-    }
-    if(pwm_width_right > 100)
-    {
-        pwm_width_right = 100;    
-    }  
-
-    //to protect TA7291P
-    if(motor_direction_left == 1)
-    {
-        IN1_left = 0;
-        IN2_left = 0;
-        wait(0.0001); //wait 100 usec    
-    }
-    if(motor_direction_right == 1)
-    {
-        IN1_right = 0;
-        IN2_right = 0;
-        wait(0.0001); //wait 100 usec    
-    }
-
-    //reverse
-    IN1_left  = 0;
-    IN2_left  = 1;
-    IN1_right = 0;
-    IN2_right = 1;
-
-    motor_left.pulsewidth_us(pwm_width_left);
-    motor_right.pulsewidth_us(pwm_width_right);
-    motor_direction_left  = 2;    
-    motor_direction_right = 2;   
-}
-
-void move_turn_right()
-{
-    //calculate the absolute value
-    pwm_width_right = -1 * pwm_width_right;
-
-    //over voltage
-    if(pwm_width_left > 100)
-    {
-        pwm_width_left = 100;    
-    }
-    if(pwm_width_right > 100)
-    {
-        pwm_width_right = 100;    
-    }  
-
-    //to protect TA7291P
-    if(motor_direction_left == 2)
-    {
-        IN1_left = 0;
-        IN2_left = 0;
-        wait(0.0001); //wait 100 usec    
-    }    
-    if(motor_direction_right == 1)
-    {
-        IN1_right = 0;
-        IN2_right = 0;
-        wait(0.0001); //wait 100 usec    
-    }
-
-    //turn right
-    IN1_left  = 1;
-    IN2_left  = 0;
-    IN1_right = 0;
-    IN2_right = 1;
-
-    motor_left.pulsewidth_us(pwm_width_left);
-    motor_right.pulsewidth_us(pwm_width_right);
-    motor_direction_left  = 1;
-    motor_direction_right = 2;   
-}
-
-void move_turn_left()
-{
-    //calculate the absolute value
-    pwm_width_left = -1 * pwm_width_left;
-
-    //over voltage
-    if(pwm_width_left > 100)
-    {
-        pwm_width_left = 100;    
-    }
-    if(pwm_width_right > 100)
-    {
-        pwm_width_right = 100;    
-    }  
-
-    //to protect TA7291P
-    if(motor_direction_left == 1)
-    {
-        IN1_left = 0;
-        IN2_left = 0;
-        wait(0.0001); //wait 100 usec    
-    }
-    if(motor_direction_right == 2)
-    {
-        IN1_right = 0;
-        IN2_right = 0;
-        wait(0.0001); //wait 100 usec    
-    }    
-
-    //turn left
-    IN1_left  = 0;
-    IN2_left  = 1;
-    IN1_right = 1;
-    IN2_right = 0;
-
-    motor_left.pulsewidth_us(pwm_width_left);
-    motor_right.pulsewidth_us(pwm_width_right);
-    motor_direction_left  = 2;    
-    motor_direction_right = 1;
 }
